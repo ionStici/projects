304f298950ed083a10171d8cd73be271fa8a6592
Ion Stici <ionsticidev@gmail.com>
2025-05-01 08:39:05 +0200

    Add projects data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+import { projects as projectsData } from "./data/projects";
+
 export default function App() {
-  return <div>Hello World</div>;
+  const [projects, setProjects] = useState(projectsData);
+  const [perPage, setPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(projects.length / perPage);
+
+  const startIndex = (currentPage - 1) * perPage;
+  const endIndex = startIndex + perPage;
+
+  const currentProjects = projects.slice(startIndex, endIndex);
+
+  return (
+    <div>
+      {currentProjects.map((project) => {
+        return <div key={project.id}>{project.title}</div>;
+      })}
+    </div>
+  );
 }
